a55f394cc25c412856ec178bd1c3128f9e4b87a2
alexeikrumshyn <alexei.w.k@gmail.com>
2020-04-13 22:20:38 -0400

    Working schedule display for a given date

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Teams from './components/teams';
+import Schedule from './components/schedule';
 
+//Main screen js code
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
+          <h1 className="App-title">Hockey Betting Pool</h1>
         </header>
+		<Schedule />
         <Teams />
       </div>
     );
