46bf1dbd8e61ed98f347af966a17722f35e2b7ae
alexeikrumshyn <alexei.w.k@gmail.com>
2021-01-10 17:02:38 -0500

    On server side, establish template for making periodic calls to retrieve the scores and adjust user points.

--- server.js
+++ server.js
@@ -3,27 +3,46 @@ var router = express.Router();
 var request = require('request');
 
 const app = express();
-/*
-app.get('https://statsapi.web.nhl.com/api/v1/teams', (req, res) => {
-  
-  write('HELOOOOO');
-  console.log(req.teams);
-  const teams = req.teams;
+const cronJob = require('cron').CronJob;
+const fetch = require('node-fetch');
 
-  res.json(teams);
-});
-*/
-/*
-router.get('/', function(req, res, next) {
-  request({
-    uri: 'https://statsapi.web.nhl.com/api/v1/teams',
-    //qs: {
-      //api_key: '123456',
-      //query: 'World of Warcraft: Legion'
-    //}
-  }).pipe(res);
-});
-*/
+//input: date object, returns string in YYYY-MM-DD format
+//note: month + 1 because date objects use Jan as month 0
+function formatDate(d) {
+  let month = '' + (d.getMonth()+1);
+  let day = '' + d.getDate();
+  let year = d.getFullYear();
+
+  if (month.length < 2) 
+	month = '0' + month;
+  if (day.length < 2) 
+	day = '0' + day;
+	
+  return [year, month, day].join('-');
+}
+
+function getGameResults() {
+	let yesterdayDate = new Date()
+	yesterdayDate.setDate(yesterdayDate.getDate() - 1)
+
+	fetch('https://statsapi.web.nhl.com/api/v1/schedule/?date='+formatDate(yesterdayDate))
+	  .then(response => response.json())
+	  .then(data => {
+		console.log(data)
+	  })
+	  .catch(err => {
+		console.log(err)
+	  })
+}
+getGameResults() //move this inside job
+
+//const job = new cronJob('*/5 * * * * *', () => {
+const job = new cronJob('0 5 * * *', () => {
+    console.log('This will get executed at 5am daily');
+}, null, true, 'Europe/London');
+
+job.start();
+
+
 const port = 5000;
-
 app.listen(port, () => `Server running on port ${port}`);
