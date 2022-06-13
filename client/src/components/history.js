dfeb2861c4df8c10c4f85e150ee9c489b2e788a6
alexeikrumshyn <alexei.w.k@gmail.com>
2022-06-12 21:20:06 -0400

    Allow user to export all previous bets in csv format

--- client/src/components/history.js
+++ client/src/components/history.js
@@ -7,6 +7,7 @@ import firebase from "firebase/app";
 
 // Add the Firebase services that you want to use
 import "firebase/database";
+import "firebase/firestore";
 
 class History extends Component {
   constructor() {
@@ -70,7 +71,46 @@ class History extends Component {
 		});
 	return response
   }
+
+  async getAllBets() {
+	const response = await firebase.database().ref('/bets/').once('value')
+		.then((snapshot) => {
+			return snapshot.val()
+		});
+	return response
+  }
   
+  //today's string in YYYY-MM-DD format
+  getTodayDate() {
+	//return '2021-01-22'
+	return this.formatDate(new Date(firebase.firestore.Timestamp.now().toDate()))
+  }
+
+  async exportBets() {
+	let bets = await this.getAllBets()
+	let today = this.getTodayDate()
+
+	//define the heading for each row of the data  
+	let csvString = 'Date,Name,Team,Amount\n'
+	for (let date in bets) {
+		if (date == today) {
+			continue
+		}
+		for (let betID in bets[date]) {
+			let usrname = this.state.users[bets[date][betID]['userID']]['firstname'] + ' ' + this.state.users[bets[date][betID]['userID']]['lastname']
+			csvString += date+','+usrname+','+bets[date][betID]['team']+','+bets[date][betID]['amount']+'\n'
+		}
+	}
+	
+	var hiddenElement = document.createElement('a');  
+	hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvString);  
+	hiddenElement.target = '_blank';  
+		
+	//provide the name for the CSV file to be downloaded  
+	hiddenElement.download = 'bet_history.csv';  
+	hiddenElement.click();  
+  }
+
   render() {
     return (	  
 	  	  
@@ -99,6 +139,7 @@ class History extends Component {
 			  </tr>
 			)}
 		</table>
+		<button onClick={() => { this.exportBets()}}>Download Full Bet History</button>
 		</div>
 	  )
     }
